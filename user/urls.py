88f32350e8fceac988e995343b6a13daec18688c
박재욱 <parkjaewook@bagjaeug-ui-Macmini.local>
2023-08-13 15:28:18 +0900

    사용자 및 관리자 기본 회원가입&로그인 API 구현

--- user/urls.py
+++ user/urls.py
@@ -1,12 +1,13 @@
 from django.urls import path
-# from .views import SignupAPIView, LoginAPIView, LogoutAPIView, UserAPIView, RefreshAPIView
+from .views import SignupAPIView, LoginAPIView, UserAPIView, RefreshAPIView, AdminLoginAPIView, AdminSignupAPIView
 from django.conf import settings
 from django.conf.urls.static import static
 
 urlpatterns = [
-    # path('signup', SignupAPIView.as_view(), name='signup'),        # 회원가입
-    # path('login', LoginAPIView.as_view(), name='login'),          # 로그인
-    # path('logout', LogoutAPIView.as_view(), name='logout'),         # 로그아웃
-    # path('user-info', UserAPIView.as_view(), name='user-info'),        # 유저 정보 조회/업데이트
-    # path('refresh', RefreshAPIView.as_view(), name='refresh'),      # refresh_token으로 토큰 갱신
+    path('signup', SignupAPIView.as_view(), name='signup'),                     # 회원가입
+    path('login', LoginAPIView.as_view(), name='login'),                        # 로그인
+    path('info', UserAPIView.as_view(), name='user-info-update'),               # 사용자 정보 수정
+    path('refresh', RefreshAPIView.as_view(), name='refresh-login'),            # refresh 로그인
+    path('admin-signup', AdminSignupAPIView.as_view(), name='admin-signup'),    # 관리자 계정 회원가입
+    path('admin-login', AdminLoginAPIView.as_view(), name='admin-login'),       # 관리자 계정 로그인
 ]  + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
--- user/views.py
+++ user/views.py
@@ -1,137 +1,211 @@
-# from django.shortcuts import render
-# from rest_framework import status
-# from rest_framework.response import Response
-# from rest_framework.views import APIView
-# from rest_framework.exceptions import APIException
-# from rest_framework.authentication import get_authorization_header
-# from .authentication import create_access_token, create_refresh_token, decode_access_token, decode_refresh_token, access_token_exp
-# from .serializer import UserSerializer
-# from .models import CustomAbstractBaseUser
+from rest_framework import status
+from rest_framework.response import Response
+from rest_framework.views import APIView
+from rest_framework.exceptions import APIException
+from rest_framework.authentication import get_authorization_header
+from .authentication import create_access_token, create_refresh_token, access_token_exp, decode_access_token, decode_refresh_token
+from .serializer import UserSerializer
+from .models import CustomAbstractBaseUser
 
 # # Create your views here.
 
-# # 회원가입 에러 처리
-# class SignupException(APIException):
-#     status_code = 400
-#     default_detail = '실패! 입력한 정보를 다시 확인해주세요'
-#     default_code = 'KeyNotFound'
+# 회원가입 에러 처리
+class SignupException(APIException):
+    status_code = 400
+    default_detail = '실패! 입력한 정보를 다시 확인해주세요'
+    default_code = 'KeyNotFound'
 
-# # 로그인 에러 처리 
-# class LoginException(APIException):
-#     status_code = 400
-#     default_detail = '아이디 혹은 비밀번호를 다시 확인해주세요'
-#     default_code = 'KeyNotFound'
+# 로그인 에러 처리 
+class LoginException(APIException):
+    status_code = 400
+    default_detail = '아이디 혹은 비밀번호를 다시 확인해주세요'
+    default_code = 'KeyNotFound'
 
-# class TokenErrorException(APIException):
-#     status_code = 403
-#     if status_code == 403:
-#         status_code = 401
-#     default_detail = 'unauthenticated'
+class EmailException(APIException):
+    status_code = 400
+    default_detail = '올바른 이메일 형식이 아닙니다. @를 입력해주세요.'
+    default_code = 'KeyNotFound'
 
-# # 로그인시 토큰 생성 함수
-# def token_create(user):    
-#     access_token = create_access_token(user.id)
-#     access_exp = access_token_exp(access_token)
-#     refresh_token = create_refresh_token(user.id)
+class PasswordException(APIException):
+    status_code = 400
+    default_detail = '비밀번호를 8자리 이상 입력해주세요'
+    default_code = 'KeyNotFound'
 
-#     response = Response(status=status.HTTP_201_CREATED)
-#     response.set_cookie(key='refreshToken', value=refresh_token, httponly=True)         #리프레쉬 토큰 쿠키에 저장
-#     response.data = {
-#         'access_token' : access_token,
-#         'access_exp' : access_exp,
-#         'refresh_token' : refresh_token
-#     }
-#     return response
+class TokenErrorException(APIException):
+    status_code = 403
+    if status_code == 403:
+        status_code = 401
+    default_detail = 'unauthenticated'
 
-# # 회원가입 API (회원가입시 즉시 로그인)
-# class SignupAPIView(APIView):
-#     def post(self, request):
-#         serializer = UserSerializer(data=request.data)
-#         if serializer.is_valid(raise_exception=True):
-#             serializer.save()
+# 관리자 로그인 에러 처리
+class AdminException(APIException):
+    status_code = 400
+    default_detail  = '관리자 계정이 아닙니다.'
+    default_code = 'KeyNotFound'
 
-#             user = User.objects.filter(username=request.data['username']).first()
-            
-#             # 중복되는 유저가 있는지 확인
-#             if not user:
-#                 raise SignupException()
-#             if not user.check_password(request.data['password']):
-#                 raise SignupException()
+# 로그인/회원가입시 토큰 생성 함수
+def token_create(user):    
+    access_token = create_access_token(user.id)
+    access_exp = access_token_exp(access_token)
+    refresh_token = create_refresh_token(user.id)
 
-#             # 가입한 유저 토큰 생성 및 로그인
-#             return token_create(user)
+    response = Response(status=status.HTTP_201_CREATED)
+    response.set_cookie(key='refreshToken', value=refresh_token, httponly=True)         #리프레쉬 토큰 쿠키에 저장
+    response.data = {
+        'access_token' : access_token,
+        'access_exp' : access_exp,
+        'refresh_token' : refresh_token
+    }
+    return response
 
-# # 로그인 API
-# class LoginAPIView(APIView):
-#     def post(self, request):
-#         user = User.objects.filter(username=request.data['username']).first()
-#         if not user:
-#             raise LoginException()
-#         if not user.check_password(request.data['password']):
-#             raise LoginException()
+# 사용자 일반 회원가입 API (회원가입시 즉시 로그인)
+class SignupAPIView(APIView):
+    def post(self, request):
+        # email 유효성 검사 : @ 포함 
+        if "@" not in request.data['email']:
+            raise EmailException()
+        # password 유효성 검사 : 8자 이상
+        if len(request.data['password']) <= 8 :
+            raise PasswordException()
         
-#         return token_create(user)
+        serializer = UserSerializer(data=request.data)
+        if serializer.is_valid(raise_exception=True):
+            serializer.save()
 
-# # 로그아웃 API
-# class LogoutAPIView(APIView):
-#     def delete(self, _):
-#         response = Response()
-#         response.delete_cookie(key="refreshToken")
-#         response.data = {
-#             'Message' : 'Logout success'
-#         }
-#         return response
-
-# # auth token 복호화 : id return
-# def token_decode(auth):
-#     token = auth[1].decode('utf-8')
-#     id = decode_access_token(token)
-#     return id
-
-# # 유저정보 조회 및 수정 API
-# class UserAPIView(APIView):
-#     def get(self, request, **kwargs):
-#         auth = get_authorization_header(request).split()
-#         if auth and len(auth) == 2:
-#             user = User.objects.filter(pk=token_decode(auth)).first()
+            user = CustomAbstractBaseUser.objects.filter(email=request.data['email']).first()
             
-#             user_serializer = UserSerializer(user).data
-#             user_id = user_serializer['id']
-#             nickname = user_serializer['nickname']
-#             profile =  user_serializer['profile']
+            # 중복되는 유저가 있는지 확인
+            if not user:
+                raise SignupException()
+            if not user.check_password(request.data['password']):
+                raise SignupException()
 
-#             response = Response()
-#             response.data = {
-#                 'id' : user_id,
-#                 'nickname' : nickname,
-#                 'profile' : profile
-#             }
-#             return response
-#         raise TokenErrorException()
-#     # 유저정보 수정
-#     def patch(self, request):
-#         auth = get_authorization_header(request).split()
-#         if auth and len(auth) == 2:
-#             user = User.objects.filter(pk=token_decode(auth)).first()
-#             serializer = UserSerializer(user, data=request.data, partial=True)
+            # 가입한 유저 토큰 생성 및 로그인
+            return token_create(user)
 
-#             if serializer.is_valid():
-#                 serializer.save()
-#                 return Response(serializer.data, status=status.HTTP_200_OK)
-#             else:
-#                 return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
-#         raise TokenErrorException()   
-# # refresh login
-# class RefreshAPIView(APIView):
-#     def post(self, request):
-#         token = request.data['refresh_token']
+# 사용자 일반 로그인 API
+class LoginAPIView(APIView):
+    def post(self, request):
+        # email 유효성 검사 : @ 포함 
+        if "@" not in request.data['email']:
+            raise EmailException()
+        # password 유효성 검사 : 8자 이상
+        if len(request.data['password']) <= 8 :
+            raise PasswordException()
         
-#         byt_token = bytes(token, 'utf-8')
+        user = CustomAbstractBaseUser.objects.filter(email=request.data['email']).first()
+        
+        # 사용자의 회원 정보가 있는지 확인      
+        if not user:
+            raise LoginException()
+        
+        # 사용자의 패스워드가 존재하는지 확인
+        if not user.check_password(request.data['password']):
+            raise LoginException()
+        
+        return token_create(user)
 
-#         id = decode_refresh_token(byt_token)
-#         access_token = create_access_token(id)
-#         access_exp = access_token_exp(access_token)
-#         return Response({
-#             'access_token': access_token,
-#             'access_exp': access_exp
-#         })
+# 로그아웃 API
+class LogoutAPIView(APIView):
+    def delete(self, _):
+        response = Response(status=status.HTTP_200_OK)
+        try:
+            response.delete_cookie(key="refresh_token")
+            response.data = {
+                'Message' : 'Logout success'
+            }
+            return response
+        except status.HTTP_400_BAD_REQUEST:
+            Response({'Message':'Logout failed'}, status=status.HTTP_400_BAD_REQUEST)
+
+# auth token 복호화 : id return
+def token_decode(auth):
+    token = auth[1].decode('utf-8')
+    id = decode_access_token(token)
+    return id
+
+# 사용자 정보 수정 API
+class UserAPIView(APIView):
+
+    # 유저정보 수정
+    def patch(self, request):
+        auth = get_authorization_header(request).split()
+        if auth and len(auth) == 2:
+            user = CustomAbstractBaseUser.objects.filter(pk=token_decode(auth)).first()
+            serializer = UserSerializer(user, data=request.data, partial=True)
+
+            if serializer.is_valid():
+                serializer.save()
+                return Response(serializer.data, status=status.HTTP_200_OK)
+            else:
+                return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+        raise TokenErrorException()
+
+# refresh login
+class RefreshAPIView(APIView):
+    def post(self, request):
+        token = request.data['refresh_token']
+        
+        byt_token = bytes(token, 'utf-8')
+
+        id = decode_refresh_token(byt_token)
+        access_token = create_access_token(id)
+        access_exp = access_token_exp(access_token)
+        return Response({
+            'access_token': access_token,
+            'access_exp': access_exp
+        })
+
+# 관리자 회원가입 API 
+class AdminSignupAPIView(APIView):
+    def post(self, request):
+        # email 유효성 검사 : @ 포함 
+        if "@" not in request.data['email']:
+            raise EmailException()
+        # password 유효성 검사 : 8자 이상
+        if len(request.data['password']) <= 8 :
+            raise PasswordException()
+        
+        # admin_check defalt 값을 True로 변경 -> 관리자
+        if request.data['admin_check'] != 'True':
+            raise AdminException()
+        
+        serializer = UserSerializer(data=request.data)
+        if serializer.is_valid(raise_exception=True):
+            serializer.save()
+
+            user = CustomAbstractBaseUser.objects.filter(email=request.data['email']).first()
+            
+            # 중복되는 유저가 있는지 확인
+            if not user:
+                raise SignupException()
+            if not user.check_password(request.data['password']):
+                raise SignupException()
+
+            # 가입한 유저 토큰 생성 및 로그인
+            return token_create(user)
+        
+class AdminLoginAPIView(APIView):
+    def post(self, request):
+        # email 유효성 검사 : @ 포함 
+        if "@" not in request.data['email']:
+            raise EmailException()
+        # password 유효성 검사 : 8자 이상
+        if len(request.data['password']) <= 8 :
+            raise PasswordException()
+        
+        user = CustomAbstractBaseUser.objects.filter(email=request.data['email']).first()
+        
+        # 사용자의 회원 정보가 있는지 확인      
+        if not user:
+            raise LoginException()
+        
+        # 사용자의 패스워드가 존재하는지 확인
+        if not user.check_password(request.data['password']):
+            raise LoginException()
+        
+        # 관리자인지 확인
+        if user.admin_check == False:
+            raise AdminException()
+        
+        return token_create(user)
